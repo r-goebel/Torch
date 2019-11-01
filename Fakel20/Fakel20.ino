02d0cc32997a5d0b56e179bc2b2736f7cdf9031f
Rebecca <r.v.d.bongard@gmail.com>
2019-11-01 23:43:49 +0100

    added Plasma effect

--- Fakel20/Fakel20.ino
+++ Fakel20/Fakel20.ino
@@ -10,6 +10,8 @@
 #define PixelPin D2
 
 int NumPixel = 300;
+int ROWS = 19;
+int COLS = 16;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NumPixel, PixelPin, NEO_GRB + NEO_KHZ800);
 
@@ -36,7 +38,8 @@ void loop() {
   //theaterChaseRainbow(50);
   //Fire(); //noch anpassen
   //meteorRain(255,255,255,4,64,true,30); //noch anpassen
-  Rain(false);
+  //Rain(false);
+  Plasma();
 }
 
 void setAll(byte red, byte green, byte blue){
@@ -380,4 +383,68 @@ void Rain(boolean WindOn){
   }
   strip.show();
   delay(30);
+}
+
+//******Plasma
+//Taken from https://github.com/johncarl81/neopixelplasma/blob/master/neopixelplasma.ino
+
+const float phaseIncrement = 0.08;  // Controls the speed of the moving points. Higher == faster. I like 0.08 .
+const float colorStretch = 0.11;    // Higher numbers will produce tighter color bands. I like 0.11 .
+float phase = 0.0;
+struct Point {
+  float x;
+  float y;
+};
+
+void Plasma() {
+  phase += phaseIncrement;
+  
+  // The two points move along Lissajious curves, see: http://en.wikipedia.org/wiki/Lissajous_curve
+  // We want values that fit the LED grid: x values between 0..13, y values between 0..8 .
+  // The sin() function returns values in the range of -1.0..1.0, so scale these to our desired ranges.
+  // The phase value is multiplied by various constants; I chose these semi-randomly, to produce a nice motion.
+  Point p1 = { (sin(phase*1.000)+1.0) * 4.5, (sin(phase*1.310)+1.0) * 4.0 };
+  Point p2 = { (sin(phase*1.770)+1.0) * 4.5, (sin(phase*2.865)+1.0) * 4.0 };
+  Point p3 = { (sin(phase*0.250)+1.0) * 4.5, (sin(phase*0.750)+1.0) * 4.0 };
+  
+  byte row, col;
+  
+  // For each row...
+  for( row=0; row<ROWS; row++ ) {
+    float row_f = float(row);  // Optimization: Keep a floating point value of the row number, instead of recasting it repeatedly.
+    
+    // For each column...
+    for( col=0; col<COLS; col++ ) {
+      float col_f = float(col);  // Optimization.
+      
+      // Calculate the distance between this LED, and p1.
+      Point dist1 = { col_f - p1.x, row_f - p1.y };  // The vector from p1 to this LED.
+      float distance1 = sqrt( dist1.x*dist1.x + dist1.y*dist1.y );
+      
+      // Calculate the distance between this LED, and p2.
+      Point dist2 = { col_f - p2.x, row_f - p2.y };  // The vector from p2 to this LED.
+      float distance2 = sqrt( dist2.x*dist2.x + dist2.y*dist2.y );
+      
+      // Calculate the distance between this LED, and p3.
+      Point dist3 = { col_f - p3.x, row_f - p3.y };  // The vector from p3 to this LED.
+      float distance3 = sqrt( dist3.x*dist3.x + dist3.y*dist3.y );
+      
+      // Warp the distance with a sin() function. As the distance value increases, the LEDs will get light,dark,light,dark,etc...
+      // You can use a cos() for slightly different shading, or experiment with other functions. Go crazy!
+      float color_1 = distance1;  // range: 0.0...1.0
+      float color_2 = distance2;
+      float color_3 = distance3;
+      float color_4 = (sin( distance1 * distance2 * colorStretch )) + 2.0 * 0.5;
+      
+      // Square the color_f value to weight it towards 0. The image will be darker and have higher contrast.
+      color_1 *= color_1 * color_4;
+      color_2 *= color_2 * color_4;
+      color_3 *= color_3 * color_4;
+      color_4 *= color_4;
+ 
+      // Scale the color up to 0..7 . Max brightness is 7.
+      strip.setPixelColor(col + (COLS * row), strip.Color(color_1, color_2, color_3));
+    }
+  }
+  strip.show();
 }
