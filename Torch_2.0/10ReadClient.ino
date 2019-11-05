da8c31dcfa90101cc72325fa1cb2f7524396548e
Rebecca Goebel <rebecca.goebel@fgdo.de>
2019-11-05 16:32:56 +0100

    started from scratch for updated version of torchcode, parallelisation

---
+++ Torch_2.0/10ReadClient.ino
@@ -0,0 +1,157 @@
+
+//Definition of Messages sent to client
+char* Messages[] = {"Welcome",
+                    "To list available effects, type 'help'.",
+                    "Please select effect: ",
+                    "Succesfully selected effect.",
+                    "Input did not match any effect.",
+                    "Do you wish to select color (y/n)?",
+                    "Selected default color.",
+                    "Please select color: ",
+                    "Succesfully selected color.",
+                    "Input did not match any color.",
+                    "Effect is changed now.",
+                    "Available effects:",
+                    "Available colors:",
+                    "To list available colors, type 'help'"};
+
+int clientStatus;
+int i;
+                        
+bool ReadClient(bool EffectChange){
+  if (!client.connected()){ 
+  //no client is connected: try to connect new one, ClientStatus = 0, return to loop
+    clientStatus = 0;
+    client = server.available();
+  }
+  else { //client is connected
+    
+    //New client connected: Welcome-Message-Package, set ClientStatus to 1
+    if (clientStatus == 0){
+      client.println(Messages[0]);
+      client.println(Messages[1]);
+      clientStatus = 1;    
+    }
+
+    //ClientStatus 1: Ask for effect selection, set ClientStatus to 2
+    if (clientStatus == 1){
+      client.print(Messages[2]);
+      clientStatus = 2;
+    }
+
+    //ClientStatus > 2: Read client input
+    if (client.available() > 0 && clientStatus > 1) { 
+      i = 0;  
+      char incomingMessage[EffectLengthMax];  
+      while (client.available() > 0 && i < EffectLengthMax) {
+        delay(10);     
+        incomingMessage[i] = client.read();
+
+        if (incomingMessage[i] == '\n' || incomingMessage[i] == '\r'){
+          incomingMessage[i] = '\0';                    //delets last character
+          i = EffectLengthMax;
+        } 
+        i++;
+      }
+
+      //Evaluate Message:
+       
+      //ClientStatus 2: evaluate input of effect
+      if (clientStatus == 2){
+        //Input equals "help": sent Message 11, print list of effects, set clientStatus to 1
+        if (strcmp(incomingMessage, "help") == 0){
+          client.println(Messages[11]);
+          for (int k=0;k<NumberEffects;k++){
+            client.println(effectList[k]);
+          }
+          clientStatus = 1;
+        }
+        //Input is something else, check list of effects for Match
+        else{
+          int Match = 0;
+          for (int k=0;k<NumberEffects;k++){
+          //Input equals effect name: Change value of SelectedNew[0], sent Message 3&5, set clientStatus to 3
+            if (strcmp(incomingMessage,effectList[k]) == 0){
+              SelectedNew[0] = k;
+              Match = 1;
+              client.println(Messages[3]);
+              client.print(Messages[5]);
+              clientStatus = 3;
+            }
+          }
+          //Input does not match any effect: sent Message 4&1, set clientSatus to 1
+          if (Match == 0){
+            client.println(Messages[4]);
+            client.println(Messages[1]);
+            clientStatus = 1;
+          }
+        }           
+      }//End of if clientStatus = 2
+        
+      //ClientStatus 3: evaluate input for color-selection (y/n)
+      else if (clientStatus == 3){
+        
+        //Input equals "n": Change value in SelectNew[1] to default color, sent Message 6&10, chnage EffectChange to 1, set clientStatus to 1
+        if (strcmp(incomingMessage, "n") == 0){
+          SelectedNew[1] = colorDefault;
+          client.println(Messages[6]);
+          client.println(Messages[10]);
+          EffectChange = 1;
+          clientStatus = 1;
+        }
+        
+        //Input equals "y": sent Message 13&7, set clientStatus to 4
+        else if (strcmp(incomingMessage, "y") == 0){
+          client.println(Messages[13]);
+          client.print(Messages[7]);
+          clientStatus = 4;
+        }
+
+        //Input does not match y or n: sent specific Message and Message 5 
+        else {
+          client.println("Unknown input.");
+          client.println(Messages[5]);            
+        }
+      }//End of if clientStatus = 3
+        
+      //ClientStatus 4: evaluate input of color
+      else if (clientStatus = 4){
+        
+        //Input equals "help": sent Message 12&7, print list of colors
+        if (strcmp(incomingMessage, "help") == 0){
+          client.println(Messages[12]);
+          for (int k=0;k<NumberColors;k++){
+            client.println(colorList[k]);
+          }
+          client.print(Messages[7]);
+        }
+
+        //Input is something else, check list of colors for Match
+        else{
+          int Match = 0;
+          for (int k=0;k<NumberColors;k++){
+          //Input equals color name: Change value of SelectedNew[1], sent Message 8&10, chnage EffectChange to 1,set clientStatus to 1
+            if (strcmp(incomingMessage,colorList[k]) == 0){
+              SelectedNew[1] = k;
+              Match = 1;
+              client.println(Messages[8]);
+              client.println(Messages[10]);
+              EffectChange = 1;
+              clientStatus = 1;
+            }
+          }
+          
+          //Input does not match any color: sent Message 9&13&7
+          if (Match == 0){
+            client.println(Messages[9]);
+            client.println(Messages[13]);
+            client.print(Messages[7]);
+          }
+        }          
+      }//End of if clientStatus = 4
+    }//End of if client.available and clientStatus > 1
+  }//end of else (client connected)
+  
+  //return EffectChange (only set to 1 if effect/color was changed, otherwise equals input value)
+  return EffectChange;
+}
---
+++ Torch_2.0/Torch_2.0.ino
@@ -0,0 +1,68 @@
+//Board: Wemos D1 R2 & mini
+//Uploadspeed:115200
+
+//Include necessary libraries
+#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+//Definition of Neopixelstrip-Variables:
+#define PixelPin D2
+int NumPixel = 300;
+int NumRows = 19;
+int NumCols = 16;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NumPixel, PixelPin, NEO_GRB + NEO_KHZ800);
+
+//Definition of possible effects and colors
+char* effectList[] = {"Fade","Cyclon"}; //,"Twinkle","TwinkleRandom","Sparkle","ColorWipe","RainbowCycle","TheaterChase","TheaterChaseRainbow","Fire","MeteorRain","Rain","Plasma"};
+int NumberEffects = 2; //13;
+char* colorList[] ={"red","green","blue"};
+int colorListRGB[][3] = {{255,0,0},
+                         {0,255,0},
+                         {0,0,255}};
+int NumberColors = 3; 
+
+//Definition of various variables
+int colorDefault = 1;         //default color if nothing is selected by client
+int Selected[] = {1,0};    //initial definition of selected effect and color
+int SelectedNew[] = {1,0}; //initial definition of new selected effect and color   
+bool EffectChange = 0;        //variable indication change in effect (0=no change, 1=change)  
+int EffectLengthMax = 20;     //Length of longes effect-name in effectList
+
+//Definition of Wifi variables (ssid and password are defined in external file)
+int port = 5333;
+WiFiServer server(port);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WifiSetup();
+  
+  //initialization of strip
+  strip.begin();
+  strip.setBrightness(10);
+  strip.show();
+}
+
+WiFiClient client;
+
+void loop() {
+  //Read input of client if availabel
+  EffectChange = ReadClient(EffectChange);
+  
+  //client selected new effect: replace effect and color in "Selected", initialize new effect if needed, reset "EffectChange"
+  if (EffectChange == 1){
+    Serial.println("EffectChange notized");
+    Selected == SelectedNew;
+    EffectChange = 0;
+    //Initialization
+  }
+  //client did not select anything new or no client is availabel: just update effect
+  else{
+    //Update
+  }
+}
