f52a0b96f33de06b6921ffabd1d6d88330c15d8e
Rebecca Goebel <rebecca.goebel@fgdo.de>
2019-11-05 21:16:51 +0100

    added effect FadeInOut

--- Torch_2.0/Effects.h
+++ Torch_2.0/Effects.h
@@ -11,30 +11,37 @@ enum effect {Fade_InOut, Rainbow_Cycle};
 //Supported directions:
 enum  direction { FORWARD, REVERSE };
 
-class Effects : public Adafruit_NeoPixel
+class Effects : public Adafruit_NeoPixel //Class Effects includes class Adafruit_Neopixel
 {
   public: 
-    Effects();
+    Effects(uint16_t pixels, uint8_t pin, uint8_t type);
 
     //public effects:
     void Update();
-    void FadeInOut(byte red, byte green, byte blue);
+    void FadeInOut(uint32_t color1, uint8_t interval, direction dir = FORWARD);
     void FadeInOutUpdate();
     void rainbowCycle(uint8_t interval, direction dir = FORWARD);
     void rainbowCycleUpdate();
 
     //helper functions
+    uint8_t Red(uint32_t color);
+    uint8_t Green(uint32_t color);
+    uint8_t Blue(uint32_t color);
     uint32_t Wheel(byte WheelPos);
 
   private:
     effect ActiveEffect;       // which pattern is running
-    direction Direction;         // direction to run the pattern
+    direction Direction;       // direction to run the pattern
     
     //Definition of private variables:
-    unsigned long Interval;         // milliseconds between updates
-    unsigned long lastUpdate;     // last update of position
-    uint16_t Index;      // current step within the pattern
+    unsigned long Interval;   // milliseconds between updates
+    unsigned long lastUpdate; // last update of position
+    uint16_t Index;           // current step within the pattern
     uint16_t TotalSteps;      // total number of steps in the pattern
+  
+    uint32_t Color1, Color2;
+    uint8_t r, g, b;
+    uint8_t red, green, blue;
 
     //Definition of privat helper functions:
     void Increment();
--- Torch_2.0/Torch_2.0.ino
+++ Torch_2.0/Torch_2.0.ino
@@ -4,6 +4,7 @@
 //Include necessary libraries
 #include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
+#include "Effects.h"
 
 #ifdef __AVR__
 #include <avr/power.h>
@@ -14,7 +15,7 @@
 int NumPixel = 300;
 int NumRows = 19;
 int NumCols = 16;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NumPixel, PixelPin, NEO_GRB + NEO_KHZ800);
+Effects strip = Effects(NumPixel, PixelPin, NEO_GRB + NEO_KHZ800);
 
 //Definition of possible effects and colors
 char* effectList[] = {"Fade","Cyclon"}; //,"Twinkle","TwinkleRandom","Sparkle","ColorWipe","RainbowCycle","TheaterChase","TheaterChaseRainbow","Fire","MeteorRain","Rain","Plasma"};
@@ -29,7 +30,7 @@ int NumberColors = 3;
 int colorDefault = 1;         //default color if nothing is selected by client
 int Selected[] = {1,0};    //initial definition of selected effect and color
 int SelectedNew[] = {1,0}; //initial definition of new selected effect and color   
-bool EffectChange = 0;        //variable indication change in effect (0=no change, 1=change)  
+bool EffectChange = 1;        //variable indication change in effect (0=no change, 1=change), initially one change necessary  
 int EffectLengthMax = 20;     //Length of longes effect-name in effectList
 
 //Definition of Wifi variables (ssid and password are defined in external file)
@@ -53,16 +54,18 @@ WiFiClient client;
 void loop() {
   //Read input of client if availabel
   EffectChange = ReadClient(EffectChange);
-  
+
   //client selected new effect: replace effect and color in "Selected", initialize new effect if needed, reset "EffectChange"
   if (EffectChange == 1){
     Serial.println("EffectChange notized");
     Selected == SelectedNew;
     EffectChange = 0;
+    strip.clear();
     //Initialization
+    strip.FadeInOut(strip.Color(255,0,0), 5);
   }
   //client did not select anything new or no client is availabel: just update effect
   else{
-    //Update
+    strip.Update();
   }
 }
