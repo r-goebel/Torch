1f67b64108178addf4f721ede628d4544a330f2a
r-goebel <rebecca.goebel@fgdo.de>
2021-11-28 12:12:08 +0100

    changed torch to platformio and homie 30

--- Code-Platformio/src/main.cpp
+++ Code-Platformio/src/main.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
-//Board: Wemos D1 R2 & mini
-//Uploadspeed:115200
 
 //Include necessary libraries
-#include <ESP8266WiFi.h>
+//#include <ESP8266WiFi.h>
 #include <Homie.h>
 #include <Effects.h>
-
-#ifdef __AVR__
-#include <avr/power.h>
-#endif
+#include <ArduinoOTA.h>
 
 //Definition of Neopixelstrip-Variables:
 #define PixelPin D2
@@ -17,6 +12,10 @@ int NumPixel = 300;
 int NumRows = 19;
 int NumCols = 16;
 Effects strip = Effects(NumPixel, PixelPin, NEO_GRB + NEO_KHZ800);
+
+//Definition of HomieNode
+HomieNode EffectNode("EffectNode", "EffectNode", "Effect");
+HomieNode ColorNode("ColorNode", "ColorNode", "Color");
 
 //Definition of possible effects and colors
 char* effectList[] = {"fade","cylon","twinkle", "twinkle random", "sparkle", "sparkle random", "color wipe", "rainbow cycle", "theater", "theater rainbow", "fire", "ice fire", "meteor rain", "rain", "marble", "heart beat"};
@@ -40,24 +39,187 @@ int SelectedNew[] = {random(0,NumberEffects),random(0,NumberColors)};    //initi
 bool EffectChange = 1;        //variable indication change in effect (0=no change, 1=change), initially one change necessary  
 int EffectLengthMax = 20;     //Length of longes effect-name in effectList
 
-//Definition of Wifi variables (ssid and password are defined in external file)
-int port = 5333;
-WiFiServer server(port);
-
 //Definition of Switch
 #define PinSwitch D6
 int intervalSwitch = 500; //interval in which no new Switch is allowed
 int lastSwitch;
 
+
+//++++++++++++++++++++++++++++++++++++
+//Homie-Loop Functions
+//++++++++++++++++++++++++++++++++++++
+bool effectHandler(const HomieRange& range, const String& effect){
+  //effect in Liste raussuchen
+  for (int i=0; i<NumberEffects; i++) {
+    if (effect == effectList[i]) {
+      SelectedNew[0] = i;
+      break;
+    }
+    else {
+      SelectedNew[0] = 0;
+    }
+  }
+  EffectChange = 1;
+  EffectNode.setProperty("effect").send(effect);
+
+  return true;
+}
+
+bool colorHandler(const HomieRange& range, const String& color){
+  //color in Liste raussuchen
+  for (int i=0; i<NumberColors; i++) {
+    if (color == colorList[i]) {
+      SelectedNew[1] = i;
+      break;
+    }
+    else {
+      SelectedNew[1] = 0;
+    }
+  }
+  EffectChange = 1;
+  ColorNode.setProperty("color").send(color);
+
+  return true;
+}
+
+//++++++++++++++++++++++++++++++++++++
+//OTA Functions
+//++++++++++++++++++++++++++++++++++++
+void OTASetup(){
+  //Setup off OTA, must be called during setup
+  //Equals Basic OTA example
+  ArduinoOTA.onStart([]() {
+  String type;
+  if (ArduinoOTA.getCommand() == U_FLASH) {
+    type = "sketch";
+  } else { // U_FS
+    type = "filesystem";
+  }
+
+  // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+  Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+}
+
+//++++++++++++++++++++++++++++++++++++
+//Helperfunctions
+//++++++++++++++++++++++++++++++++++++
+void SelectEffect (){
+  if (Selected[0] == 0){
+    strip.FadeInOut(colorListRGB[Selected[1]],10); 
+    
+  } else if (Selected[0] == 1){
+    strip.Cylon(colorListRGB[Selected[1]], 4, 10);
+    
+  } else if (Selected[0] == 2){ //Twinkle
+    strip.Twinkle(colorListRGB[Selected[1]],10,250);
+       
+  }else if (Selected[0] == 3){ //TwinkleRandom
+    strip.Twinkle(colorListRGB[Selected[1]],10,250,1);
+    
+  }else if (Selected[0] == 4){ //Sparkle
+    strip.Sparkle(colorListRGB[Selected[1]],0);
+    
+  }else if (Selected[0] == 5){ //SparkleRandom
+    strip.Sparkle(colorListRGB[Selected[1]],0,1);
+    
+  }else if (Selected[0] == 6){ //ColorWipe
+    strip.colorWipe(colorListRGB[Selected[1]],50);
+    
+  } else if (Selected[0] == 7){ //RainbowCycle
+    strip.rainbowCycle(10);
+    
+  } else if (Selected[0] == 8){ //TheaterChase
+    strip.theaterChase(colorListRGB[Selected[1]]);
+    
+  } else if (Selected[0] == 9){ //TheaterChaseRainbow
+    strip.theaterChase(colorListRGB[Selected[1]],1);
+    
+  } else if (Selected[0] == 10){ //Fire
+    strip.fireSpiral(NumCols);
+    
+  } else if (Selected[0] == 11){ //Ice Fire
+    strip.fireSpiral(NumCols,1,100,175);
+      
+  } else if (Selected[0] == 12){ //Meteor Rain
+    strip.meteorRainSpiral(colorListRGB[Selected[1]],NumCols);
+  
+  } else if (Selected[0] == 13){ //Rain without wind
+    strip.rainSpiral(colorListRGB[Selected[1]],0,NumCols);
+  
+  }else if (Selected[0] == 14){ //Marble
+    strip.rollingMarble(10);
+  
+  }else if (Selected[0] == 15){ //Heart Beat
+    strip.heartBeat(colorListRGB[Selected[1]]);
+  
+  }
+}
+
+bool SwitchState;
+
+bool CheckSwitch(){
+  //Check switch state
+  SwitchState = digitalRead(PinSwitch);
+    
+  //if Sensor value exceeds limit
+  if(SwitchState == HIGH){
+    //Change Effect randonly
+    SelectedNew[0] = random(NumberEffects-1);
+    SelectedNew[1] = random(NumberColors-1);
+    EffectChange = 1;
+    EffectNode.setProperty("effect").send(effectList[SelectedNew[0]]);
+    ColorNode.setProperty("color").send(colorList[SelectedNew[1]]);
+  }
+    
+  else{
+    EffectChange = 0;
+  }
+
+  return EffectChange;
+}
+
+//++++++++++++++++++++++++++++++++++++
+//Main Functions
+//++++++++++++++++++++++++++++++++++++
+
 void setup() {
-  Homie_setFirmware("torch", "1.2.0"); // The underscore is not a typo! See Magic bytes
+  //Homie
+  Homie_setFirmware("Torch", "1.0.0"); // The underscore is not a typo! See Magic bytes
+  //Homie.setLoopFunction(loopHandler);
+  //Node.advertise("PropertyID").setName("PropertyName").setDatatype("datatype").setUnit("unit");
+  EffectNode.advertise("effect").setName("Effect").setDatatype("String").settable(effectHandler);
+  ColorNode.advertise("color").setName("Color").setDatatype("String").settable(colorHandler);
+
   Homie.setup();
+
+  OTASetup();
+  ArduinoOTA.begin();
   
   Serial.begin(115200);
 
   delay(1000);
 
-  WifiSetup();
   
   //initialization of strip
   strip.begin();
@@ -65,15 +227,12 @@ void setup() {
   strip.show();
 }
 
-WiFiClient client;
-
 void loop() {
+  //Read input via Homie/Dashboard
   Homie.loop();
-  //Read input of client if availabel
-  EffectChange = ReadClient(EffectChange);
+  ArduinoOTA.handle();
 
-
-  //if no input was recived from client, check switch
+  //if no input was recived from homie, check switch
   if (EffectChange == 0 && millis()-lastSwitch > intervalSwitch){
     EffectChange = CheckSwitch();
 
@@ -82,7 +241,7 @@ void loop() {
        }
   }
 
-  //client selected new effect: replace effect and color in "Selected", initialize new effect if needed, reset "EffectChange"
+  //new effect selected: replace effect and color in "Selected", initialize new effect if needed, reset "EffectChange"
   if (EffectChange == 1){
     Serial.println("EffectChange noticed");
     Selected[0] = SelectedNew[0];
